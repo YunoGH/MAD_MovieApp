177e6118a4124fb68f4277ed962dc454623f0ffc
YunoGH <89008852+YunoGH@users.noreply.github.com>
2024-03-10 20:24:54 +0100

    Abgabe Lab02 MAD

--- app/src/main/java/com/example/movieappmad24/ui/theme/Color.kt
+++ app/src/main/java/com/example/movieappmad24/ui/theme/Color.kt
@@ -9,3 +9,7 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val Blue80 = Color(0xFF1BD2E9)
+val Blueish80 = Color(0xFF8CEEFA)
+val Blueishy80 = Color(0xFFA0C9CE)
--- app/src/main/java/com/example/movieappmad24/ui/theme/Theme.kt
+++ app/src/main/java/com/example/movieappmad24/ui/theme/Theme.kt
@@ -16,15 +16,19 @@ import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    //primary = Purple80,
+    //secondary = PurpleGrey80,
+    //tertiary = Pink80
+
+    primary = Blue80,
+    secondary = Blueish80,
+    tertiary = Blueishy80
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = Blue80,
+    secondary = Blueish80,
+    tertiary = Blueishy80
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
